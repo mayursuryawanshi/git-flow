e2af290bedcbfc1c62faff2f3dac6b7180a26926
mayursuryawanshi <suryamayur34@gmail.com>
2025-01-28 15:12:36 +0530

    some change

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-    <h1>Hello World, Once again </h1>
+    {/* <h1>Hello World, Once again </h1> */}
     <p>
       This is a simple Vite + React template with TypeScript.
     </p>
